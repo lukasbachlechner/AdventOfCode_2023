68ab5624c579b8a9871aacc4a1050ef52b5a0f0a
Lukas Bachlechner <lukas.bachlechner@marqably.com>
2023-12-02 18:17:01 +0100

    day01: finish

--- solutions/day01.dart
+++ solutions/day01.dart
@@ -1,3 +1,5 @@
+import 'dart:collection';
+
 import '../utils/index.dart';
 
 class Day01 extends GenericDay {
@@ -10,12 +12,30 @@ class Day01 extends GenericDay {
 
   @override
   int solvePart1() {
-    return parseInput()
+    return sumFirstAndLastNumbersInEachLine(parseInput());
+  }
+
+  @override
+  int solvePart2() {
+    final normalizedInput = parseInput().map(replaceWordsWithNumbers).toList();
+    return sumFirstAndLastNumbersInEachLine(
+      normalizedInput,
+    );
+  }
+
+  /// Creates a two digit number from the first and last number in each line and
+  /// sums them up.
+  int sumFirstAndLastNumbersInEachLine(List<String> normalizedInput) {
+    return normalizedInput
         .map((line) {
+          // Find all single digits in the line
           final numbersInLine = RegExp('[0-9]{1}')
               .allMatches(line)
               .map((match) => match.group(0));
 
+          // Create a two digit number from the first and last number in the
+          // line. If there is only one number in the line, the second number
+          // will be null and therefore ignored by the whereNotNull() call.
           return [
             numbersInLine.firstOrNull,
             numbersInLine.lastOrNull,
@@ -25,8 +45,38 @@ class Day01 extends GenericDay {
         .reduce((a, b) => a + b);
   }
 
-  @override
-  int solvePart2() {
-    return 0;
+  Map<String, int> get numberMap => {
+        'one': 1,
+        'two': 2,
+        'three': 3,
+        'four': 4,
+        'five': 5,
+        'six': 6,
+        'seven': 7,
+        'eight': 8,
+        'nine': 9,
+      };
+
+  String replaceWordsWithNumbers(String line) {
+    // Using a splay tree map to keep the resulting numbers sorted by their
+    // original index int the input line
+    final result = SplayTreeMap<int, int>();
+
+    // Iterate over all words and numbers in the line
+    for (final entry in numberMap.entries) {
+      // Create a regex that matches either the word or the number
+      final regex = RegExp('${entry.key}|${entry.value}');
+
+      // Iterate over all matches and add them to the result map with the
+      // original index as key
+      for (final match in regex.allMatches(line)) {
+        // We can be sure that we only have one match per index, because there
+        // can be no overlapping matches (like e. g. 'nine' and 'ninety')
+        result[match.start] = entry.value;
+      }
+    }
+
+    // Return the resulting string by joining the values of the map
+    return result.values.join();
   }
 }
